23ab72336fbf9fc7e63f96db5a1fee7929039e26
siyu811 <77983231+siyu811@users.noreply.github.com>
2022-06-04 17:21:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,4 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git Jerryk https://github.com/jerrykuku/openwrt-package.git' feeds.conf.default
-sed -i '$a src-git netspeedtest  https://github.com/sirpdboy/netspeedtest' feeds.conf.default
+sed -i '$a src-git netspeedtest https://github.com/sirpdboy/netspeedtest' feeds.conf.default
